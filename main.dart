b53d243819825b572deea8b9153c157608216d11
mahnoor20070208 <154058860+mahnoor20070208@users.noreply.github.com>
2024-01-07 04:04:15 -0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,21 +1,12 @@
-import 'dart:io';
-
-void main(){
-  //write a program that prints the multiplication table of a given number using a for loop
-print("Multiplication table for $num:");
-  for (int i = 1; i <= 10; i++) {
-    int result = num*i;
-    print("$num x $i = $result");
-    //create a flutter loop that prints even numbers from 2 to 10
-    print("Even numbers from 2 to 10:");
-
-  for (int i = 2; i <= 10; i += 2) {
-    print(i);
-  }
-  //implement a code that check whether a given number is prime or not
-  if (bool(num)) {
-    print("$num is a prime number.");
-  } else {
-    print("$num is not a prime number.");
-  }
+void main (){
+printhelloparammm("mujtaba", "mahnoor");
+printhelloparame(msg: "hi", sub: "hello");
+}
+printhelloparammm(msg,sub){
+  print (msg);
+  print(sub);
+}
+printhelloparame({required msg, required sub}){
+  print (msg);
+  print(sub);
 }
