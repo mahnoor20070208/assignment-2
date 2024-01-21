deb1fe1fc87fe074dab0f61a6eab7318d351974d
mahnoor20070208 <mahnoorfima@gmail.com>
2024-01-20 23:31:53 -0800

    origin

--- main.dart
+++ main.dart
@@ -1,21 +1,22 @@
-import 'dart:io';
-
-void main(){
-  //write a program that prints the multiplication table of a given number using a for loop
-print("Multiplication table for $num:");
-  for (int i = 1; i <= 10; i++) {
-    int result = num*i;
-    print("$num x $i = $result");
-    //create a flutter loop that prints even numbers from 2 to 10
-    print("Even numbers from 2 to 10:");
-
-  for (int i = 2; i <= 10; i += 2) {
-    print(i);
+void main (){
+  student student1 = student("mujtaba");
+  student1.loving();
+var sm1 =SmartPhone.android();
+}
+class student {
+  var name = "";
+  var name2 = "mahnoor";
+  student(a){
+    name = a;
   }
-  //implement a code that check whether a given number is prime or not
-  if (bool(num)) {
-    print("$num is a prime number.");
-  } else {
-    print("$num is not a prime number.");
+  loving(){
+    
   }
 }
+  class SmartPhone{
+SmartPhone();
+SmartPhone.android(){
+  print("is loving");
+}
+SmartPhone.ios();
+  }
