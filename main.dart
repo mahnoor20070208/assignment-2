07c1d78b3b6b4f26a7823755665877c5e6f5980c
mahnoor20070208 <154058860+mahnoor20070208@users.noreply.github.com>
2024-01-20 23:27:03 -0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,12 +1,22 @@
 void main (){
-printhelloparammm("mujtaba", "mahnoor");
-printhelloparame(msg: "hi", sub: "hello");
+  student student1 = student("mujtaba");
+  student1.loving();
+var sm1 =SmartPhone.android();
 }
-printhelloparammm(msg,sub){
-  print (msg);
-  print(sub);
+class student {
+  var name = "";
+  var name2 = "mahnoor";
+  student(a){
+    name = a;
+  }
+  loving(){
+    
+  }
 }
-printhelloparame({required msg, required sub}){
-  print (msg);
-  print(sub);
+  class SmartPhone{
+SmartPhone();
+SmartPhone.android(){
+  print("is loving");
 }
+SmartPhone.ios();
+  }
